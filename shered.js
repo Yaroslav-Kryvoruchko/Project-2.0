88cc33c64ba9e900ac4e114abc215266319871e7
andrewkryvoruchko <andrew.kryvoruchko@gmail.com>
2020-04-15 15:39:27 +0300

    index.html

--- shered.js
+++ shered.js
@@ -2,12 +2,21 @@ var OpenButton = document.querySelector('.toggle-button');
 var MobileBackground = document.querySelector('.mobile-background');
 var MobileMenu = document.querySelector('.mobile-menu');
 
+
 OpenButton.addEventListener('click', function() {
-    MobileBackground.classList.add("open")
     MobileMenu.classList.add("open");
+    MobileBackground.style.display = "block";
+  setTimeout(function() {
+    MobileBackground.classList.add ("open");
+  }, 10);
 });
 
 MobileBackground.addEventListener('click', function(){
     MobileBackground.classList.remove("open")
     MobileMenu.classList.remove("open")
+    MobileBackground.style.display = "none";
+  setTimeout(function() {
+    MobileBackground.classList.remove("open");
+  }, 200);
 });
+
